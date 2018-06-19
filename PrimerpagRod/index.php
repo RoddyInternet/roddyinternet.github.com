d24bf0b1a4e2a4b28a2e0b322e41413204b2fd8e
Roddy <roddy.tellamo@gmail.com>
2018-06-19 15:41:54 -0600

    Primera pagina
    la primera

---
+++ PrimerpagRod/index.php
@@ -0,0 +1,192 @@
+<!doctype html>
+<head>
+	<meta charset="utf-8">
+	<title>Musica Urbana</title>
+</head>
+<body style="background-color:#282828;font-family: Quicksand;margin: 0;">
+	<header style="margin-left: auto;margin-right: auto; background-color: black;color: white;text-align: center; padding:16px;">
+		<h1 style="display: inline-block;width: 400px;font-weight: 1600px;">Music</h1>
+		<h2 style="display: inline-block;width: 400px;font-weight: 200px;">I like that world</h2>
+
+	</header><!-- /header -->
+	<div style="background-color: #1db954;text-align: center;font-weight: 1000px; padding: 9px;">
+		Mis palabras son tan cortas, que necesito MUSICA para expresarte mi sentir.
+	</div>
+	<section>
+	<div style="margin-right: auto;margin-left: 20%;">
+		<div style="display: inline-block;width:65%;vertical-align: top;">
+			<h3 style="font-size: 36px;font-weight: 900;color: white;">Generos</h3>
+			<dl style="color: white;">
+				<dt style="font-size: 30px;font-weight: 200px;text-transform: uppercase;margin-top: 36px;">R&b</dt>
+				<dd>El R&B combina elementos del hip hop, del soul, del rhythm and blues, del pop y del funk. ¡Una mezcla explosiva!</dd>
+				<dt style="font-size: 30px;font-weight: 200px;text-transform: uppercase;margin-top: 36px;">Trap</dt>
+				<dd>El trap es un subgénero que inicialmente pertenecía al ramo del hip hop del sur de Estados Unidos y que con los años se incorporó a la electrónica a el hip hop comercial y más recientemente al reggaeton. Existe una pequeña, pero notable, diferencia entre cada una de esas variedades de trap, especialmente la electrónica que también se denomina EDM trap (Electronic Dance Music).</dd>
+				<dt style="font-size: 30px;font-weight: 200px;text-transform: uppercase;margin-top: 36px;">Reggaeton</dt>
+				<dd>Es un género musical bailable​ que tiene sus raíces en la música de América Latina y el Caribe.​ Su sonido deriva del reggae jamaicano, influido por el hip hop.</dd>
+			</dl>
+		</div>
+		<div style="display: inline-block;width:10%;margin-top:20px;">
+			<img src="/PrimerpagRod/images/generos.png"/>
+		</div>
+	</div>
+	</section>
+
+	<section>
+		<div style="display: block;margin-top: 4em;margin-bottom: 4em;margin-left: auto;margin-right: auto;border-style: inset;border-width: 1px;background-color:#535353;
+	margin-right: auto;margin-left: auto;"></div>
+		<div  style="margin-right: auto;margin-left: auto;color: black;">
+		<div>
+		<h3 style="font-size: 36px; font-weight: 900; color: white;margin-left: 10%">Artistas Rnb</h3>
+		<ol  style="color: #1db954; font-size: 26px; text-align: center;margin-right: 4%;">
+			<li style="display: inline-block;width:275px;">Chis Brown</li>
+			<li style="display: inline-block;width:275px;">Rihanna</li>
+			<li style="display: inline-block;width:275px;">Usher</li>
+		</ol>
+		<div style="text-align: center;">
+			<img src="/PrimerpagRod/images/cb.png" style="display: inline-block;width: 21%" />
+			<img src="/PrimerpagRod/images/ri.png" style="display: inline-block;width: 21%" />
+			<img src="/PrimerpagRod/images/ush.png" style="display: inline-block;width: 21%" />
+		</div>
+		<div style="margin-right: 4%;">
+		<ol  style="background-color:#333333; color: white;font-size: 26px;text-align: center;">
+			<li style="display: inline-block;width:275px;">Party</li>
+			<li style="display: inline-block;width:275px;">Crazy Friday</li>
+			<li style="display: inline-block;width:275px;">Back to Sleep</li>
+		</ol>
+		<ol  style="color: white;font-size: 26px;text-align: center;">
+			<li style="display: inline-block;width:275px;">Loyalty</li>
+			<li style="display: inline-block;width:275px;">Wild Thoughts</li>
+			<li style="display: inline-block;width:275px;">Too Good</li>
+		</ol>
+		<ol  style="color: white;font-size: 26px;text-align: center;">
+			<li style="display: inline-block;width:275px;">Yeah</li>
+			<li style="display: inline-block;width:275px;">My Boo</li>
+			<li style="display: inline-block;width:275px;">New Flame</li>
+		</ol>
+		</div>
+		</div>
+		</div>
+
+	</section>
+	<section>
+		<div style="display: block;margin-top: 4em;margin-bottom: 4em;margin-left: auto;margin-right: auto;border-style: inset;border-width: 1px;background-color:#535353;
+	margin-right: auto;margin-left: auto;"></div>
+		<div  style="margin-right: auto;margin-left: auto;">
+		<div>
+		<h3 style="font-size: 36px;font-weight: 900;color: white;margin-left: 10%">Artistas Trap</h3>
+		<ol  style="color: #1db954;font-size: 26px;text-align: center; margin-right: 4%;">
+			<li style="display: inline-block;width:275px;">Jon Z</li>
+			<li style="display: inline-block;width:275px;">Bad Bunny</li>
+			<li style="display: inline-block;width:275px;">Arcangel</li>
+		</ol>
+		<div style="text-align: center;">
+			<img src="/PrimerpagRod/images/jonz.png" style="display: inline-block;width: 21%" />
+			<img src="/PrimerpagRod/images/bd.png" style="display: inline-block;width: 21%" />
+			<img src="/PrimerpagRod/images/arca.png" style="display: inline-block;width: 21%" />
+		</div>
+		<div style="margin-right: 4%;">
+		<ol  style="color: white;font-size: 26px;text-align: center;">
+			<li style="display: inline-block;width:275px;">Mis diamantes</li>
+			<li style="display: inline-block;width:275px;">Viajo sin ver</li>
+			<li style="display: inline-block;width:275px;">0 Sentimientos</li>
+		</ol>
+		<ol  style="background-color:#333333; color: white;font-size: 26px;text-align: center;">
+			<li style="display: inline-block;width:275px;">Tu no vive así</li>
+			<li style="display: inline-block;width:275px;">Chambea</li>
+			<li style="display: inline-block;width:275px;">Sensualidad</li>
+		</ol>
+		<ol  style="color: white;font-size: 26px;text-align: center;">
+			<li style="display: inline-block;width:275px;">Tacos Altos</li>
+			<li style="display: inline-block;width:275px;">Te robo rmx</li>
+			<li style="display: inline-block;width:275px;">Rojo</li>
+		</ol>
+		</div>
+		</div>
+		</div>
+	</section>
+	<section>
+		<div style="display: block;margin-top: 4em;margin-bottom: 4em;margin-left: auto;margin-right: auto;border-style: inset;border-width: 1px;background-color:#535353;
+	margin-right: auto;margin-left: auto;"></div>
+		<div  style="margin-right: auto;margin-left: auto;">
+		<div>
+		<h3 style="font-size: 36px;font-weight: 900;color: white;margin-left: 10%">Artistas Reggaeton</h3>
+		<ol  style="color: #1db954;font-size: 26px;text-align: center; margin-right: 4%;">
+			<li style="display: inline-block;width:275px;">Daddy Yankee</li>
+			<li style="display: inline-block;width:275px;">Ozuna</li>
+			<li style="display: inline-block;width:275px;">Karol G</li>
+		</ol>
+		<div style="text-align: center;">
+			<img src="/PrimerpagRod/images/dy.png" style="display: inline-block;width: 21%" />
+			<img src="/PrimerpagRod/images/ozu.png" style="display: inline-block;width: 21%" />
+			<img src="/PrimerpagRod/images/kg.png" style="display: inline-block;width: 21%" />
+		</div>
+		<div style="margin-right: 4%;">
+		<ol  style=" color: white;font-size: 26px;text-align: center;">
+			<li style="display: inline-block;width:275px;">Dura</li>
+			<li style="display: inline-block;width:275px;">Gasolina</li>
+			<li style="display: inline-block;width:275px;">Hula Hoop</li>
+		</ol>
+		<ol  style="color: white;font-size: 26px;text-align: center;">
+			<li style="display: inline-block;width:275px;">Balenciaga</li>
+			<li style="display: inline-block;width:275px;">Siguelo Bailando</li>
+			<li style="display: inline-block;width:275px;">Criminal</li>
+		</ol>
+		<ol  style="background-color:#333333;color: white;font-size: 26px;text-align: center;">
+			<li style="display: inline-block;width:275px;">Pinneaple</li>
+			<li style="display: inline-block;width:275px;">Tu pum pum</li>
+			<li style="display: inline-block;width:275px;">La Dama</li>
+		</ol>
+		</div>
+		</div>
+		</div>
+	</section>
+
+<div style="background-color: black;padding-bottom: 10vh; padding-top: 10vh; margin-top: 10vh;">
+	<div style="margin-left: 20%;margin-right: auto;">
+		<h4 style="color: green; display: inline-block;width:200px;">GENEROS</h4>
+		<h4 style="color: green; display: inline-block;width:200px;">TOP</h4>
+		<h4 style="color: green; display: inline-block;width:200px;">CUENTA</h4>
+		<h4 style="color: green; display: inline-block;width:200px;">REDES SOCIALES</h4>
+	</div>
+
+	<div style="margin-top:-4vh; margin-left: 20%;margin-right: auto;">
+		<h5 style="color: white; display: inline-block;width:200px;">R&B</h5>
+		<h5 style="color: white; display: inline-block;width:200px;">Las mejores 10</h5>
+		<h5 style="color: white; display: inline-block;width:200px;">Cerrar Sesión</h5>
+		<h5 style="color: white; display: inline-block;width:200px;">Facebook</h5>
+	</div>
+
+	<div style="margin-top:-4vh; margin-left: 20%;margin-right: auto;">
+		<h5 style="color: white; display: inline-block;width:200px;">Latin Trap</h5>
+		<h5 style="color: white; display: inline-block;width:200px;">Las mejores 50</h5>
+		<h5 style="color: white; display: inline-block;width:200px;">Editar perfil</h5>
+		<h5 style="color: white; display: inline-block;width:200px;">Twitter</h5>
+	</div>
+
+	<div style="margin-top:-4vh; margin-left: 20%;margin-right: auto;">
+		<h5 style="color: white; display: inline-block;width:200px;">Reggaeton</h5>
+		<h5 style="color: white; display: inline-block;width:200px;">Solo las 100</h5>
+		<h5 style="color: white; display: inline-block;width:200px;">Enlazar</h5>
+		<h5 style="color: white; display: inline-block;width:200px;">Instagram</h5>
+	</div>
+
+	<div style="margin-top:-4vh; margin-left: 20%;margin-right: auto;">
+		<h5 style="color: white; display: inline-block;width:200px;">Freestyles</h5>
+		<h5 style="color: white; display: inline-block;width:200px;"></h5>
+		<h5 style="color: white; display: inline-block;width:200px;">Inbox</h5>
+		<h5 style="color: white; display: inline-block;width:200px;"></h5>
+	</div>
+
+	<div style="margin-top: 5vh; margin-right: auto; margin-left: auto;">
+		<p style="margin-top:-1vh;color: white;text-align: center;font-size: small;">Top de lo mejor....</p>
+		<p style="margin-top:-1vh;color: white;text-align: center;font-size: small;">Kins music</p>
+		<p style="margin-top:-1vh;color: white;text-align: center;font-size: small;">2018</p>
+	</div>
+	<div style="margin-right: auto; margin-left: auto;" >
+		<img style="width:5%;margin-top:1vh; margin-left: 47%;margin-right: auto;" src="/PrimerpagRod/images/king.png"/>
+	</div>
+</div>
+
+
+
+</body>
